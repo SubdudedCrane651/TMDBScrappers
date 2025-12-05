e4c91daad8ab2fabad685f44602a31165777b1a0
SubduedCrane651 <rchrdperreault@gmail.com>
2025-12-05 04:25:08 -0500

    Fixed the ShowImageAndInfo vba code to have a textbox for cast also

--- ShowImageAndInfo.vba
+++ ShowImageAndInfo.vba
@@ -2,23 +2,23 @@ Sub ShowImageAndInfo()
     ' Make sure Tools > References includes:
     ' - Microsoft Visual Basic for Applications Extensibility 5.3
     ' - Microsoft Forms 2.0 Object Library
-    
+
     Dim btnName As String
     btnName = Application.Caller
-    
+
     Dim ws As Worksheet
     Set ws = ThisWorkbook.Sheets("Sheet1")
-    
+
     Dim rowIndex As Long
     rowIndex = ws.Shapes(btnName).TopLeftCell.Row
-    
+
     Dim imageURL As String
     imageURL = ws.Cells(rowIndex, 9).Value
-    
+
     If imageURL <> "N/A" Then
         Dim localFilePath As String
         localFilePath = DownloadImage(imageURL)
-        
+
         If localFilePath <> "" Then
             ' Create the UserForm dynamically
             Dim VBComp As VBComponent
@@ -28,7 +28,7 @@ Sub ShowImageAndInfo()
                 .Properties("Height") = 600
                 .Properties("Caption") = ws.Cells(rowIndex, 2).Value
             End With
-            
+
             ' Add Image control
             Dim ImgControl As MSForms.Image
             Set ImgControl = VBComp.Designer.Controls.Add("Forms.Image.1")
@@ -39,7 +39,7 @@ Sub ShowImageAndInfo()
                 .Height = 300
                 .Picture = LoadPicture(localFilePath)
             End With
-            
+
             ' Add Labels for Description, Director, Writer, Cast
             Dim txtDesc As MSForms.TextBox
             Set txtDesc = VBComp.Designer.Controls.Add("Forms.TextBox.1")
@@ -55,7 +55,7 @@ Sub ShowImageAndInfo()
                 .Locked = True ' makes it read-only
                 .BackColor = RGB(240, 240, 240) ' optional styling
             End With
-            
+
             Dim lblDirector As MSForms.Label
             Set lblDirector = VBComp.Designer.Controls.Add("Forms.Label.1")
             With lblDirector
@@ -64,7 +64,7 @@ Sub ShowImageAndInfo()
                 .Top = 370
                 .Width = 380
             End With
-            
+
             Dim lblWriter As MSForms.Label
             Set lblWriter = VBComp.Designer.Controls.Add("Forms.Label.1")
             With lblWriter
@@ -73,17 +73,22 @@ Sub ShowImageAndInfo()
                 .Top = 420
                 .Width = 380
             End With
-            
-            Dim lblCast As MSForms.Label
-            Set lblCast = VBComp.Designer.Controls.Add("Forms.Label.1")
-            With lblCast
-                .Caption = "Cast: " & ws.Cells(rowIndex, 8).Value
+
+            Dim txtCast As MSForms.TextBox
+            Set txtCast = VBComp.Designer.Controls.Add("Forms.TextBox.1")
+            With txtCast
+                .Text = "Cast: " & ws.Cells(rowIndex, 8).Value
                 .Left = 10
                 .Top = 430
                 .Width = 380
+                .Height = 100
+                .MultiLine = True
                 .WordWrap = True
+                .ScrollBars = fmScrollBarsVertical
+                .Locked = True ' makes it read-only
+                .BackColor = RGB(240, 240, 240) ' optional styling
             End With
-            
+
             ' Show the form and clean up afterwards
             With VBA.UserForms.Add(VBComp.Name)
                 .Show
--- TMDBScrapperExcel.py
+++ TMDBScrapperExcel.py
@@ -192,23 +192,23 @@ def update_excel_with_movie_details(excel_file_path, sheet_name):
             ' Make sure Tools > References includes:
             ' - Microsoft Visual Basic for Applications Extensibility 5.3
             ' - Microsoft Forms 2.0 Object Library
-            
+
             Dim btnName As String
             btnName = Application.Caller
-            
+
             Dim ws As Worksheet
             Set ws = ThisWorkbook.Sheets("Sheet1")
-            
+
             Dim rowIndex As Long
             rowIndex = ws.Shapes(btnName).TopLeftCell.Row
-            
+
             Dim imageURL As String
             imageURL = ws.Cells(rowIndex, 9).Value
-            
+
             If imageURL <> "N/A" Then
                 Dim localFilePath As String
                 localFilePath = DownloadImage(imageURL)
-                
+
                 If localFilePath <> "" Then
                     ' Create the UserForm dynamically
                     Dim VBComp As VBComponent
@@ -218,7 +218,7 @@ def update_excel_with_movie_details(excel_file_path, sheet_name):
                         .Properties("Height") = 600
                         .Properties("Caption") = ws.Cells(rowIndex, 2).Value
                     End With
-                    
+
                     ' Add Image control
                     Dim ImgControl As MSForms.Image
                     Set ImgControl = VBComp.Designer.Controls.Add("Forms.Image.1")
@@ -229,7 +229,7 @@ def update_excel_with_movie_details(excel_file_path, sheet_name):
                         .Height = 300
                         .Picture = LoadPicture(localFilePath)
                     End With
-                    
+
                     ' Add Labels for Description, Director, Writer, Cast
                     Dim txtDesc As MSForms.TextBox
                     Set txtDesc = VBComp.Designer.Controls.Add("Forms.TextBox.1")
@@ -245,7 +245,7 @@ def update_excel_with_movie_details(excel_file_path, sheet_name):
                         .Locked = True ' makes it read-only
                         .BackColor = RGB(240, 240, 240) ' optional styling
                     End With
-                    
+
                     Dim lblDirector As MSForms.Label
                     Set lblDirector = VBComp.Designer.Controls.Add("Forms.Label.1")
                     With lblDirector
@@ -254,7 +254,7 @@ def update_excel_with_movie_details(excel_file_path, sheet_name):
                         .Top = 370
                         .Width = 380
                     End With
-                    
+
                     Dim lblWriter As MSForms.Label
                     Set lblWriter = VBComp.Designer.Controls.Add("Forms.Label.1")
                     With lblWriter
@@ -263,17 +263,22 @@ def update_excel_with_movie_details(excel_file_path, sheet_name):
                         .Top = 420
                         .Width = 380
                     End With
-                    
-                    Dim lblCast As MSForms.Label
-                    Set lblCast = VBComp.Designer.Controls.Add("Forms.Label.1")
-                    With lblCast
-                        .Caption = "Cast: " & ws.Cells(rowIndex, 8).Value
+
+                    Dim txtCast As MSForms.TextBox
+                    Set txtCast = VBComp.Designer.Controls.Add("Forms.TextBox.1")
+                    With txtCast
+                        .Text = "Cast: " & ws.Cells(rowIndex, 8).Value
                         .Left = 10
                         .Top = 430
                         .Width = 380
+                        .Height = 100
+                        .MultiLine = True
                         .WordWrap = True
+                        .ScrollBars = fmScrollBarsVertical
+                        .Locked = True ' makes it read-only
+                        .BackColor = RGB(240, 240, 240) ' optional styling
                     End With
-                    
+
                     ' Show the form and clean up afterwards
                     With VBA.UserForms.Add(VBComp.Name)
                         .Show
@@ -331,7 +336,7 @@ def update_excel_with_movie_details(excel_file_path, sheet_name):
         wb.close()
         
 # Example usage
-# excel_file_path = "My Movie Library.xlsx"
+#excel_file_path = "My Movie Library.xlsx"
 excel_file_path = "F:/Richard/My Movie Library.xlsx"
 if not os.path.isabs(excel_file_path):
    excel_file_path=os.path.join(os.path.expanduser("~"), "Documents")+"\\"+excel_file_path
